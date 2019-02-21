9b73827d3bd26e5b1983e6d011644f03d175d971
SpringLee <1028789852@qq.com>
2019-02-21 23:20:21 +0800

    commit

--- Core.MVC/Startup.cs
+++ Core.MVC/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Core.MVC
@@ -15,20 +16,45 @@ namespace Core.MVC
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddSingleton<IWelCome, Welcome>(); // 
+            //services.AddTransient<IWelCome, Welcome>();
+            //services.AddScoped<IWelCome, Welcome>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,IWelCome welcome)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            //app.Run(async (context) =>
+            //{
+            //    var wel = welcome.GetMessage();
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+
+            app.UseWelcomePage(new WelcomePageOptions() { Path = "/Home" }); 
+
         }
-    }
+
+        public interface IWelCome
+        {
+             string GetMessage();
+        }
+
+        public class Welcome : IWelCome
+        {
+            public string GetMessage()
+            {
+                return "heheda";
+            }
+        }
+
+    } 
+  
 }
