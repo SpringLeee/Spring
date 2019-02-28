48d4a9b170b21b3ed325bb554b1b7643ee511ef0
Spring Lee <1028789852@qq.com>
2019-03-01 02:17:33 +0800

    路由

---
+++ CoreWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {   
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new { code = 0, msg = "处理成功", data = "/Home/Index" });
+        } 
+
+
+        [HttpGet]
+        public IActionResult Order()
+        {
+            return Ok(new { code = 0, msg = "处理成功", data = "/Home/Order" });
+        }
+
+        [HttpPost]
+        public IActionResult Mall(MallEntity mall)
+        {
+            return Ok(new { code = 0, msg = "处理成功", data = mall });
+        }
+
+
+    }
+}
---
+++ CoreWebAPI/Models/MallEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreWebAPI.Models
+{
+    public class MallEntity
+    {
+        public string a { get; set; }
+
+        public int b { get; set; }
+
+        public string c { get; set; }
+
+    }
+}
